7e852a234b9ba2870c1810da67c11be91380137d
Myra-13 <86005779+Myra-13@users.noreply.github.com>
2021-07-27 15:27:10 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,21 @@
+class Ball{
+constructor(x,y){
+
+    var options {
+
+    
+        pointA :pointA,
+        stiffness = 0.04,
+        length = 10
+    }
+    this.ball.loadImage(rock.png);
+  
+display(){
+
+
+    
+}
+
+
+}
+
